de4326a31d9057eddfb241799bf393aae2cac4b8
Arjun Dholakiya <arjundholakiyav@gmail.com>
2026-01-29 13:07:15 +0530

    Adding more features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,43 +7,57 @@ export default function Header() {
       {/* Big heading */}
       <h1
         className="
-          text-[5.5rem]
+          text-[3.2rem]
+          sm:text-[4rem]
           md:text-[11rem]
           font-medium
           tracking-tight
-          leading-[0.85]
+          leading-[0.9]
           text-white
-          ml-30
+
+          text-center
+          md:text-left
+
+          mx-auto
+          md:mx-0
+          md:ml-30
         "
       >
         Kubernetes
       </h1>
 
-      {/* Badge — NOT inside flex */}
+      {/* Badge */}
       <span
         className="
-          absolute
-          left-[58%]
-          top-[85%]
-          flex
-          items-center
-          gap-2
-          text-[15px]
-          font-bold
-          tracking-widest
-          bg-[#89a0a7]
-          px-14
-          py-3
-          rounded-xl
-          text-[#ffffff]
-          whitespace-nowrap
-        "
+    absolute
+    left-1/2
+    -translate-x-1/2
+    top-full
+    mt-3
+
+    md:left-[58%]
+    md:top-[85%]
+    md:mt-0
+    md:translate-x-0
+
+    flex
+    items-center
+    gap-2.5
+    text-[12px]
+    md:text-[15px]
+    font-semibold
+    tracking-wide
+    bg-[#8fa6ad]
+    px-6
+    md:px-14
+    py-1.5
+    md:py-3
+    rounded-xl
+    text-white
+    whitespace-nowrap
+  "
       >
-        <img
-          src={chakra}
-          alt=""
-          className="w-4 h-4"
-        />
+        <img src={chakra} alt="" className="w-4 h-4" />
         Certified Service Provider
       </span>
     </div>
